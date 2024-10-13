862ca2698de4947a91b74ca60f628513e4e5d423
pritesh6754 <86052547+pritesh6754@users.noreply.github.com>
2024-10-13 13:32:52 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DemoDesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 != s2)
+            {
+                Console.WriteLine("Not Sharing the same resource");
+
+            }
+            else
+            {
+                Console.WriteLine("Sharing the same resource");
+            }
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoDesignPattern
+{
+    internal class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton() { }
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+
+            }
+            return instance;
+        }
+    }
+}
